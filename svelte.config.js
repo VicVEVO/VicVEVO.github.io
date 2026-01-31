185af45ce91652f27f105d7bf952ced986c52a37
VicVEVO <viclouisnoe@gmail.com>
2026-01-31 10:30:03 +0100

    v1.1

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   preprocess: [vitePreprocess()],
