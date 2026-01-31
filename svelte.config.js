8c79a62ce4bd2e77b13de162f3c739c300c0feae
VicVEVO <viclouisnoe@gmail.com>
2026-01-31 15:38:50 +0100

    v1.2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 export default {
-  preprocess: [vitePreprocess()],
-  kit: {
-	adapter: adapter()
+	preprocess: [vitePreprocess()],
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: dev ? '' : '',
+		}
 	}
 };
